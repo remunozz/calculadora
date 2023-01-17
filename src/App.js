5db08d474e0e37147c95ce2539a495703d158928
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-17 17:38:40 -0500

    Contenedor del logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
     <div className="App">
-      Hola
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        src = {freeCodeCampLogo}
+        className='freecodecamp-logo' 
+        alt='Logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
