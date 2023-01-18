92b05ae4dedb95006653f4a0359e7c2d7613c0f6
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-17 19:31:08 -0500

    estilo de los botones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
@@ -9,6 +10,16 @@ function App() {
         src = {freeCodeCampLogo}
         className='freecodecamp-logo' 
         alt='Logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
       </div>
     </div>
   );
