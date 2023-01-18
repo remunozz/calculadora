dcc2441ea62d887b81bf24fe3f73e9fca635ee25
remunozz <87038583+remunozz@users.noreply.github.com>
2023-01-18 13:40:09 -0500

    agregando condicionales en react, componente Boton.js

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -7,13 +7,31 @@ function Boton(props) {
     return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
 
-  return (
+  if(esOperador(props.children)) {
+    return (
+      <div 
+      className ='boton-contenedor operador' 
+      onClick = {() => props.manejarClick(props.children)} >
+        {props.children}
+      </div>
+    );
+  } else {
+    return (
+      <div 
+      className ='boton-contenedor' 
+      onClick = {() => props.manejarClick(props.children)} >
+        {props.children}
+      </div>
+    );
+  }
+  
+  /*return (
     <div 
     className={`boton-contenedor ${esOperador(props.children) ? 'operador': ''}`.trimEnd()} 
     onClick = {() => props.manejarClick(props.children)} >
       {props.children}
     </div>
-  );
+  );*/
 }
 
 export default Boton;
